12af8086a4c63e0906fe9a8d93b50e0260319e44
Ekaterina Chekhonina <catherinefish@yandex-team.ru>
2023-02-16 17:42:11 +0300

    поправила параметры парсинга аргументов

--- 02_PushPip/cowsay_1.py
+++ 02_PushPip/cowsay_1.py
@@ -1,27 +1,24 @@
 from cowsay import cowsay
 import argparse
+import sys
 
-parser = argparse.ArgumentParser(description='Cow say something', prog='PROG',
+parser = argparse.ArgumentParser(description='Cow say something',
     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('-e', dest='eyes', action='store_const',
-                    const='oo', default='oo',
-                    help="cow's eyes, first two chars wil be used", required=False)
-parser.add_argument('-f', dest='cowfile', action='store_const',
-                    const='', default='',
+
+parser.add_argument('-e', dest='eyes', action='store', default='oo', help="cow's eyes, first two chars wil be used", required=False)
+parser.add_argument('-f', dest='cowfile', action='store',
+                     default='',
                     help="path to cowfile", required=False)
-parser.add_argument('-n', action='store_true',
+parser.add_argument('-n', dest='is_wrap', action='store_true',
                     help="the given message will not be word-wrapped", required=False)
-parser.add_argument('-T', dest='tongue', action='store_const',
-                    const='', default='',
+parser.add_argument('-T', dest='tongue', action='store',
+                    default='',
                     help="cow's tongue, first two chars wil be used", required=False)
-parser.add_argument('-W', dest='wrap', action='store_const',
-                    const=40, default=40,
+parser.add_argument('-W', dest='wrap', action='store',
+                    default=40,
                     help="where the message should be wrapped", required=False)
-parser.add_argument('message', action='store_const',
-                    const="", default="",
+parser.add_argument('message', action='store',
+                     default="",
                     help="what cow will say")
-
-args = parser.parse_args()
-
 
 print(args)
