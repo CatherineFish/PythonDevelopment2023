ce8cb993fc3b6b4a8852acdbaf306f23d03a0914
Чехонина Екатерина <katerinasheps@gmail.com>
2023-03-04 22:24:13 +0300

    Добавила парсинг аргументов для cowsay

--- 04_MergetoolCommandline/cow_say.py
+++ 04_MergetoolCommandline/cow_say.py
@@ -1,5 +1,7 @@
 import cmd
 import cowsay
+import shlex
+import readline
 
 
 class CowSayCmd(cmd.Cmd):
@@ -21,9 +23,22 @@ class CowSayCmd(cmd.Cmd):
 		'This is the text that appears above the cows'
 		print(cowsay.make_bubble(text))
 
-	def do_cowsay(self, text):
-		'Display a message as cow phrases'
-		print(cowsay.cowsay(text))
+	def do_cowsay(self, arg):
+		'''
+		cowsay message [cow [eyes [tongue]]]
+		Display a message as cow phrases
+		'''
+		message, *options = shlex.split(arg)
+		cow = 'default'
+		eyes = 'oo'
+		tongue = '  '
+		if options:
+			cow = options[0]
+			if len(options) > 1:
+				eyes = options[1]
+				if len(options) > 2:
+					tongue = options[2]
+		print(cowsay.cowsay(message, eyes=eyes, tongue=tongue, cow=cow))
 
 	def do_cowthink(self, text):
 		'Display a message as cow thought'
