22d7f13b9265c01cc86a04959a321a2f91a33153
Чехонина Екатерина <katerinasheps@gmail.com>
2023-03-04 21:09:06 +0300

    Добавила exit

--- 04_MergetoolCommandline/cow_say.py
+++ 04_MergetoolCommandline/cow_say.py
@@ -5,5 +5,9 @@ class CowSayCmd(cmd.Cmd):
 	intro = "Say cow and enter!"
 	prompt = "moo>"
 
+	def do_exit(self, arg):
+		'End command line'
+		return True
+
 if __name__ == "__main__":
 	CowSayCmd().cmdloop()
