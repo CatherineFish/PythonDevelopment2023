89e05a47b7c0380068a2825db7ddc7beb248f1c1
Чехонина Екатерина <katerinasheps@gmail.com>
2023-03-04 21:17:20 +0300

    Добавила list_cows

--- 04_MergetoolCommandline/cow_say.py
+++ 04_MergetoolCommandline/cow_say.py
@@ -1,4 +1,5 @@
 import cmd
+import cowsay
 
 
 class CowSayCmd(cmd.Cmd):
@@ -8,6 +9,13 @@ class CowSayCmd(cmd.Cmd):
 	def do_exit(self, arg):
 		'End command line'
 		return True
+
+	def do_list_cows(self, directory=""):
+		'Lists all cow file names in the given directory'
+		if directory:
+			print(cowsay.list_cows(directory))
+		else:
+			print(cowsay.list_cows())
 
 if __name__ == "__main__":
 	CowSayCmd().cmdloop()
