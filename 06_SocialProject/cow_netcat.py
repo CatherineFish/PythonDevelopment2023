eb795a73ecc2ea06d323dcecf1bd6616aafc674d
Чехонина Екатерина <katerinasheps@gmail.com>
2023-03-23 01:26:25 +0300

    добавила отправку who на сервер

--- 06_SocialProject/cow_netcat.py
+++ 06_SocialProject/cow_netcat.py
@@ -2,12 +2,20 @@ import cmd
 import threading
 import time
 import readline
+import asyncio
 
+async def write(message, writer):
+    writer.write(f"{message}\n".encode())
+    await writer.drain()
 
-class simple(cmd.Cmd):
+class CowNetcat(cmd.Cmd):
 
-    def do_echo(self, arg):
-        print(arg)
+    def __init__(self, reader, writer):
+        super(CowNetcat, self).__init__()
+        self.reader, self.writer = reader, writer
+
+    def do_who(self, arg):
+        asyncio.run(write("who", self.writer))
 
 
 def spam(cmdline, timeout, count):
@@ -16,7 +24,15 @@ def spam(cmdline, timeout, count):
         print(f"\nI'm a message № {i}!\n{cmdline.prompt}{readline.get_line_buffer()}", end="", flush=True)
 
 
-cmdline = simple()
-timer = threading.Thread(target=spam, args=(cmdline, 3, 10))
-timer.start()
-cmdline.cmdloop()
+
+#timer = threading.Thread(target=spam, args=(cmdline, 3, 10))
+#timer.start()
+
+async def main():
+    reader, writer = await asyncio.open_connection('0.0.0.0', 1337)
+    cmdline = CowNetcat(reader, writer)
+    return cmdline
+
+
+cmd = asyncio.run(main())
+asyncio.run(cmd.cmdloop())
