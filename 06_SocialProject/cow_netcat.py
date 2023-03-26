73d95e0f93cb14a5ade77853569fc472ed10d69e
Чехонина Екатерина <katerinasheps@gmail.com>
2023-03-27 00:02:23 +0300

    Сделала первичный комплит команд: добавила использование select, тк сделала неблокирующим сокет для подключения нескольких клиентов

--- 06_SocialProject/cow_netcat.py
+++ 06_SocialProject/cow_netcat.py
@@ -4,6 +4,7 @@ import readline
 import shlex
 import socket
 import sys
+import select
 
 
 HOST = '0.0.0.0'
@@ -12,21 +13,27 @@ PORT = 1337
 
 def write(message, socket):
     socket.send(f"{message}\n".encode())
-    
 
-def recive_messages(socket):
+def get_message(socket):
+    event, *trash = select.select([socket], [], [], 0.0)
+    for sock in event:
+        return sock.recv(1024).decode().strip()
+    return ""
+
+
+def recive_messages(socket, cmdline):
     while True:
-        data = socket.recv(1024).decode().strip()
-        if not(data):
-            break
-        print(f'{data}\n{cmdline.prompt}{readline.get_line_buffer()}', end="", flush=True)
+        data = get_message(socket)
+        if data:
+            print(f'{data}\n{cmdline.prompt}{readline.get_line_buffer()}', end="", flush=True)
 
 
 class CowNetcat(cmd.Cmd):
 
-    def __init__(self, socket):
+    def __init__(self, socket, locker):
         super(CowNetcat, self).__init__()
         self.sock = socket
+        self.locker = locker
         
     def do_who(self, arg):
         write("who", self.sock)
@@ -39,7 +46,12 @@ class CowNetcat(cmd.Cmd):
         write(f"login {login}", self.sock)
 
     def complete_login(self, text, line, begidx, endidx):
-        pass      
+        current_args = shlex.split(line)
+        args_len = len(current_args)
+        if args_len <= 2:
+            with self.locker:
+                write("cows", self.sock) 
+                return [res for res in get_message(self.sock) if res.startwith(text.lower())]    
 
     def do_say(self, arg):
         cow_name, message, *trash = shlex.split(arg)
@@ -57,6 +69,7 @@ class CowNetcat(cmd.Cmd):
 
     def do_quit(self, arg):
         write("quit", self.sock)
+        exit(0)
 
         
 
@@ -71,6 +84,7 @@ if __name__ == "__main__":
             continue
         try:
             s.connect(sa)
+            s.setblocking(False)
         except OSError as msg:
             s.close()
             s = None
@@ -80,9 +94,10 @@ if __name__ == "__main__":
         print('could not open socket')
         sys.exit(1)
     with s:
+
         locker = threading.Lock()
-        cmdline = CowNetcat(s)
-        thread = threading.Thread(target=recive_messages, args=(s,))
+        cmdline = CowNetcat(s, locker)
+        thread = threading.Thread(target=recive_messages, args=(s, cmdline))
         thread.start()
         cmdline.cmdloop()
 
