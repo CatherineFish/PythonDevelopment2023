e8c2192cf56a6fc25bfff274d537cf5a91b714f1
Чехонина Екатерина <katerinasheps@gmail.com>
2023-03-26 23:05:32 +0300

    Возвращаю версию без номеров запросов, добавляю локер - буду через него делать комплит команд

--- 06_SocialProject/cow_netcat.py
+++ 06_SocialProject/cow_netcat.py
@@ -14,60 +14,49 @@ def write(message, socket):
     socket.send(f"{message}\n".encode())
     
 
+def recive_messages(socket):
+    while True:
+        data = socket.recv(1024).decode().strip()
+        if not(data):
+            break
+        print(f'{data}\n{cmdline.prompt}{readline.get_line_buffer()}', end="", flush=True)
+
 
 class CowNetcat(cmd.Cmd):
 
     def __init__(self, socket):
         super(CowNetcat, self).__init__()
         self.sock = socket
-        self.buffer_for_cows = []
-        self.buffer_for_who = []
         
     def do_who(self, arg):
-        write(f"0 who", self.sock)
-
+        write("who", self.sock)
 
     def do_cows(self, arg):
-        write(f"0 cows", self.sock)
+        write("cows", self.sock)
 
     def do_login(self, arg):
         login, *trash = shlex.split(arg)
-        write(f"0 login {login}", self.sock)
+        write(f"login {login}", self.sock)
 
     def complete_login(self, text, line, begidx, endidx):
-        current_args = shlex.split(line)
-        args_len = len(current_args)
-        if args_len <= 2:
-            write(f"1 cows", self.sock)
-    
-            return [res for res in recive_messages(self.sock) if res.startwith(text.lower())]
-
+        pass      
 
     def do_say(self, arg):
         cow_name, message, *trash = shlex.split(arg)
-        write(f"0 say {cow_name} {message}", self.sock)
+        write(f"say {cow_name} {message}", self.sock)
+
+    def complete_say(self, text, line, begidx, endidx):
+        pass  
 
     def do_yield(self, arg):
         message, *trash = shlex.split(arg)
-        write(f"0 yield {message}", self.sock)
+        write(f"yield {message}", self.sock)
+
+    def complete_yield(self, text, line, begidx, endidx):
+        pass  
 
     def do_quit(self, arg):
-        write(f"0 quit", self.sock)
-
-
-    def recive_messages(self):
-        while True:
-            data = self.soc.recv(1024).decode().strip()
-            number = shlex.split(data)[0]
-            if int(number) == 1:
-                self.buffer_for_cows = data[1:]
-                break
-            elif int(number) == 2:
-                self.buffer_for_who = data[1:]
-                break
-            if not(data):
-                break
-            print(f'RESULT {data}\n{cmdline.prompt}{readline.get_line_buffer()}', end="", flush=True)
+        write("quit", self.sock)
 
         
 
@@ -91,6 +80,7 @@ if __name__ == "__main__":
         print('could not open socket')
         sys.exit(1)
     with s:
+        locker = threading.Lock()
         cmdline = CowNetcat(s)
         thread = threading.Thread(target=recive_messages, args=(s,))
         thread.start()
