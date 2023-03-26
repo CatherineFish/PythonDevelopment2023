47aef7e5928b9111b3a37caa86b10d30822fe079
Чехонина Екатерина <katerinasheps@gmail.com>
2023-03-26 18:14:31 +0300

    Перешла с asyncio на socket

--- 06_SocialProject/cow_netcat.py
+++ 06_SocialProject/cow_netcat.py
@@ -1,57 +1,85 @@
 import cmd
 import threading
-import time
 import readline
-import sys, shlex
-import asyncio
+import shlex
+import socket
+import sys
 
-async def write(message, writer):
-    writer.write(f"{message}\n".encode())
-    await writer.drain()
+
+HOST = '0.0.0.0'
+PORT = 1337  
+
+
+def write(message, socket):
+    socket.send(f"{message}\n".encode())
+    
+
+#async def send_messages_as(reader):
+#    send = asyncio.create_task(reader.readline())
+#    while not reader.at_eof():
+#        done, pending = await asyncio.wait([send], return_when=asyncio.ALL_COMPLETED)
+#        for q in done:
+#            send = asyncio.create_task(reader.readline())
+#            print(f'{q.result().decode()}\n{cmdline.prompt}{readline.get_line_buffer()}', end="", flush=True)
+
 
 class CowNetcat(cmd.Cmd):
 
-    def __init__(self, reader, writer):
+    def __init__(self, socket):
         super(CowNetcat, self).__init__()
-        self.reader, self.writer = reader, writer
-
+        self.sock = socket
+        
     def do_who(self, arg):
-        asyncio.run(write("who", self.writer))
+        write("who", self.sock)
 
     def do_cows(self, arg):
-        asyncio.run(write("cows", self.writer))
+        write("cows", self.sock)
 
     def do_login(self, arg):
         login, *trash = shlex.split(arg)
-        asyncio.run(write(f"login {login}", self.writer))
+        write(f"login {login}", self.sock)
 
     def do_say(self, arg):
         cow_name, message, *trash = shlex.split(arg)
-        asyncio.run(write(f"say {cow_name} {message}", self.writer))
+        write(f"say {cow_name} {message}", self.sock)
 
     def do_yield(self, arg):
         message, *trash = shlex.split(arg)
-        asyncio.run(write(f"yield {message}", self.writer))
+        write(f"yield {message}", self.sock)
 
     def do_quit(self, arg):
-        asyncio.run(write("quit", self.writer))
+        write("quit", self.sock)
 
 
-def spam(cmdline, timeout, count):
-    for i in range(count):
-        time.sleep(timeout)
-        print(f"\nI'm a message № {i}!\n{cmdline.prompt}{readline.get_line_buffer()}", end="", flush=True)
+    #def send_messages(self):
+    #    send_messages_as(self.reader))
+        
 
+if __name__ == "__main__":
+    s = None
+    for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC, socket.SOCK_STREAM):
+        af, socktype, proto, canonname, sa = res
+        try:
+            s = socket.socket(af, socktype, proto)
+        except OSError as msg:
+            s = None
+            continue
+        try:
+            s.connect(sa)
+            s.setblocking(False)
+        except OSError as msg:
+            s.close()
+            s = None
+            continue
+        break
+    if s is None:
+        print('could not open socket')
+        sys.exit(1)
+    with s:
+        cmdline = CowNetcat(s)
+        cmdline.cmdloop()
+    
 
-
-#timer = threading.Thread(target=spam, args=(cmdline, 3, 10))
-#timer.start()
-
-async def main():
-    reader, writer = await asyncio.open_connection('0.0.0.0', 1337)
-    cmdline = CowNetcat(reader, writer)
-    return cmdline
-
-
-cmd = asyncio.run(main())
-cmd.cmdloop()
+    
+#sender.run()
+    
